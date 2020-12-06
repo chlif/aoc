1625c78d6a6d7a12451f57185c220aa39e3c5f50
Tuomas Paasonen <tuomas.paasonen@futurice.com>
2020-12-06 13:41:29 +0200

    Add d6p1

---
+++ 2020/day6/exercise1.js
@@ -0,0 +1,18 @@
+let fs = require('fs');
+const file = './input.txt';
+
+const uniq = group =>
+    group
+      .split('')
+      .reduce((mem, char) => (mem.includes(char)) ? mem : [...mem, char], [])
+      .join('');
+
+fs.readFile(file, 'utf8', (err, data) => {
+  if (err) return console.err(err);
+  const groups = data.split("\n\n")
+    .map(group => group.replace(/\n/g, ''))
+    .map(uniq);
+
+  console.log(groups.reduce((mem, group) => mem + group.length, 0));
+
+});
