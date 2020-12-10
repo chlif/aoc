a9f6f817a3d24039fef6ff85fbdf292365437175
Tuomas Paasonen <tuomas.paasonen@futurice.com>
2020-12-10 20:44:39 +0200

    Clean d10p2 by removing test data

--- 2020/day10/exercise2.js
+++ 2020/day10/exercise2.js
@@ -1,6 +1,5 @@
 let fs = require('fs');
 const file = './input.txt';
-const testSet = [28,33,18,42,31,14,46,20,48,47,24,23,49,45,19,38,39,11,1,32,25,35,8,17,7,9,4,2,34,10,3];
 
 const countPaths = (chargers) => {
   let pathLengths = {};
