208106facdd25e33d7e09cf8fe97fc6bb1134b7a
Tuomas Paasonen <tuomas.paasonen@futurice.com>
2021-12-05 10:17:30 +0200

    Add day 5 of 2021

---
+++ 2021/05/e01.js
@@ -0,0 +1,29 @@
+let fs = require('fs');
+const file = './input.txt';
+
+fs.readFile(file, 'utf8', (err, data) => {
+  if (err) return console.err(err);
+  const input = data.split('\n').filter(row => row.length > 0);
+  const regex = /([0-9]*),([0-9]*) \-\> ([0-9]*),([0-9]*)/;
+  const coords = data => data.map(row => row.match(regex).slice(1,5).map(i => parseInt(i,10)));
+  const isStraight = row => row[0] === row[2] || row[1] === row[3];
+  const min = (i1,i2) => (i1 < i2) ? i1 : i2;
+  const max = (i1,i2) => (i1 > i2) ? i1 : i2;
+
+  const count = (lines) => {
+    const h = lines.map(l => max(l[0],l[2])).reduce((a,i) => (i > a) ? i : a, 0)+1;
+    const w = lines.map(l => max(l[1],l[3])).reduce((a,i) => (i > a) ? i : a, 0)+1;
+    let map = Array.from(Array(h)).map(i => Array.from(Array(w)).map(j => 0));
+    lines.forEach(l => {
+      if (l[0] === l[2]) {
+        Array.from(Array(Math.abs(l[1]-l[3])+1)).map((a,i) => min(l[1],l[3])+i).forEach(y => map[l[0]][y]++)
+      } else {
+        Array.from(Array(Math.abs(l[0]-l[2])+1)).map((a,i) => min(l[0],l[2])+i).forEach(x => map[x][l[1]]++);
+      }
+    });
+    return map.reduce((a,r) => [...a,...r], []).filter(i => i > 1).length;
+  };
+
+  console.log("E1: ", count(coords(input).filter(isStraight)));
+
+});
