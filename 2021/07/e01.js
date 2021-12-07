1ac03a6f481e0a9ddc591dfde8f87b80bd673a63
Tuomas Paasonen <tuomas.paasonen@futurice.com>
2021-12-07 08:08:44 +0200

    Add day 7 of 2021

---
+++ 2021/07/e01.js
@@ -0,0 +1,22 @@
+let fs = require('fs');
+const file = './input.txt';
+
+fs.readFile(file, 'utf8', (err, data) => {
+  if (err) return console.err(err);
+  const input = data.split('\n').filter(row => row.length > 0);
+  const crabs = (d => d[0].split(',').map(i => parseInt(i)))(input);
+  const fuel = (point,c) => c.reduce((acc,b) => acc + Math.abs(point-b), 0);
+  const min = c => c.reduce((a,b) => (a === undefined || b < a) ? b : a, undefined);
+  const max = c => c.reduce((a,b) => (a === undefined || b > a) ? b : a, undefined);
+
+  const bruteFind = c => {
+    const lowest = min(c);
+    const highest = max(c);
+    return min(
+        [...Array(highest-lowest)].map((a,i) => lowest+i)
+        .map(p => fuel(p,c))
+    );
+  };
+
+  console.log("E1: ", bruteFind(crabs));
+});
