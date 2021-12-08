0ef1e05750dbdf96de2ecfe2eaf81205c1343d93
Tuomas Paasonen <tuomas.paasonen@futurice.com>
2021-12-08 14:09:17 +0200

    Create little script to help setup new day

---
+++ 2021/_/e02.js
@@ -0,0 +1,10 @@
+let fs = require('fs');
+const file = './input.txt';
+const stepRegex = /([a-z]*)\ ([0-9]*)/;
+
+fs.readFile(file, 'utf8', (err, data) => {
+  if (err) return console.err(err);
+  const input = data.split('\n').filter(row => row.length > 0);
+
+  console.log("E2: " + input);
+});
