78ca14591f3b76e31604852c3485a3014a5d22b8
Tuomas Paasonen <tuomas.paasonen@futurice.com>
2021-12-09 09:27:39 +0200

    Add day 9 of 2021

---
+++ 2021/09/e02.js
@@ -0,0 +1,35 @@
+let fs = require('fs');
+const file = './input.txt';
+const stepRegex = /([a-z]*)\ ([0-9]*)/;
+
+fs.readFile(file, 'utf8', (err, data) => {
+  if (err) return console.err(err);
+  const input = data.split('\n').filter(row => row.length > 0);
+  const w = input[0].length;
+  const field = input.reduce((a,r) => a+r, "").split("").map(i => parseInt(i));
+
+  const findAdjacentIds = (i,m) => [i-w,i-1,i+1,i+w].filter(j => j >= 0 && j < m.length && (Math.abs(j-i) > 1 || Math.floor(j/w) === Math.floor(i/w)) );
+
+  const lowpoints = f => {
+    return f.map((p,i,m) => {
+      const isLowpoint = findAdjacentIds(i,m)
+          .map(j => parseInt(m[j]))
+          .reduce((a,j) => a && j > parseInt(p), true);
+      return (isLowpoint) ? i : -1;
+    }).filter(i => i > -1);
+  };
+
+  const countBasin = (basin,idx,f) => {
+    if (idx > basin.length) return basin.length;
+    const neighboursInBasin = findAdjacentIds(basin[idx],f).filter(j => f[j] < 9).filter(j => !basin.includes(j));
+    return countBasin([...basin,...neighboursInBasin],idx+1,f);
+  }
+
+  const basins = f => {
+    return lowpoints(f).map(lp => countBasin([lp], 0, f));
+  };
+
+  const multiplyTopThree = b => b.sort((i1,i2) => i2-i1).slice(0,3).reduce((a,i) => a*i, 1);
+
+  console.log("E2: ", multiplyTopThree(basins(field)));
+});
