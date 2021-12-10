f8edc6d320904b5c80d663b723d99be222418ec5
Tuomas Paasonen <tuomas.paasonen@futurice.com>
2021-12-10 08:45:38 +0200

    Add day 10 of 2021

---
+++ 2021/10/e01.js
@@ -0,0 +1,43 @@
+let fs = require('fs');
+const file = './input.txt';
+const stepRegex = /([a-z]*)\ ([0-9]*)/;
+
+fs.readFile(file, 'utf8', (err, data) => {
+  if (err) return console.err(err);
+  const input = data.split('\n').filter(row => row.length > 0);
+
+  const scoreboard = {
+    ')': 3, ']': 57, '}': 1197, '>': 25137
+  };
+
+  const isCorrectClosingCharacter = (open, close) => {
+    switch (open) {
+      case '(':
+        return close === ')';
+      case '[':
+        return close === ']';
+      case '{':
+        return close === '}';
+      case '<':
+        return close === '>';
+      default:
+        return false;
+    }
+  };
+
+  const validate = (l,s) => {
+    if (l.length === 0) return true;
+    const c = l.charAt(0);
+    if (c === '(' || c === '[' || c === '{' || c === '<')
+      return validate(l.substring(1), [c, ...s]);
+    if (s.length > 0 && isCorrectClosingCharacter(s[0],c))
+      return validate(l.substring(1), s.slice(1));
+    return c;
+  }
+
+  const findInvalidClosingCharacters = raw => raw.map(line => validate(line, [])).filter(c => c !== true);
+
+  const score = chars => chars.reduce((a,c) => a + scoreboard[c], 0)
+
+  console.log("E1: ", score(findInvalidClosingCharacters(input)));
+});
