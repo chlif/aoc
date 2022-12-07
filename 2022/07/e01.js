0a1140c019cf0c2b5a711bc2c77b8ede12402b24
Tuomas Paasonen <tuomas.paasonen@futurice.com>
2022-12-07 08:52:50 +0200

    Add day 7/22

---
+++ 2022/07/e01.js
@@ -0,0 +1,60 @@
+let fs = require('fs');
+const file = './input.txt';
+
+fs.readFile(file, 'utf8', (err, data) => {
+  if (err) return console.err(err);
+  const input = data.split('\n').filter(row => row.length > 0);
+
+  let system = [];
+  let curr = -1;
+  input.forEach(row => {
+    const parts = row.split(' ');
+
+    if (parts[0] === '$') {
+      if (parts[1] === 'cd') {
+        if (parts[2] === '/') {
+          if (system.length === 0) {
+            system.push(buildDir('/', null));
+          }
+          curr = 0;
+        } else if (parts[2] === '..') {
+          curr = (system[curr].parent !== null) ? system[curr].parent : curr;
+        } else {
+          curr = system[curr].children.filter(c => c.name === parts[2])[0].id;
+        }
+      }
+      // Add ls logic here, if needed
+    } else {
+      if (parts[0] === 'dir') {
+        system.push(buildDir(parts[1], curr));
+      } else {
+        system.push(buildFile(parts[1], curr, parseInt(parts[0])));
+      }
+      system[curr].children.push(buildChild(parts[1], system.length-1));
+    }
+  });
+
+  for (let i = system.length-1; i >= 0; i--) {
+    if (system[i].type === 'DIR') {
+      for (let j = 0; j < system[i].children.length; j++) {
+        system[i].size += system[system[i].children[j].id].size;
+      }
+    }
+  }
+
+  let out = system
+      .filter(o => o.type === 'DIR' && o.size < 100000)
+      .reduce((a,b) => a+b.size, 0);
+
+  console.log("E1: " + out);
+});
+
+const buildDir = (name, parent) => ({
+  name, parent, size: 0, type: 'DIR', children: []
+});
+
+const buildFile = (name, parent, size) => ({
+  name, parent, size, type: 'FILE'
+});
+
+const buildChild = (name, id) => ({name, id});
