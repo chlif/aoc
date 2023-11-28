2e706f108c3200bdfea678acfe5c3a4a9be35468
Tuomas Paasonen <tuomas.paasonen@futurice.com>
2023-11-28 13:06:35 +0200

    Create base for 2023

---
+++ 2023/_/e02.js
@@ -0,0 +1,9 @@
+let fs = require('fs');
+const file = './input.txt';
+
+fs.readFile(file, 'utf8', (err, data) => {
+  if (err) return console.err(err);
+  const input = data.split('\n').filter(row => row.length > 0);
+
+  console.log("E2: " + input);
+});
