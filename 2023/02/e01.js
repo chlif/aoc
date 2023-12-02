4a45a1a5dca7ad55a16c179d41d2865d0d1a2e71
Tuomas Paasonen <tuomas.paasonen@futurice.com>
2023-12-02 07:23:24 +0200

    Add day 2 of 2023

---
+++ 2023/02/e01.js
@@ -0,0 +1,25 @@
+let fs = require('fs');
+const file = './input.txt';
+
+fs.readFile(file, 'utf8', (err, data) => {
+  if (err) return console.err(err);
+  const input = data.split('\n').filter(row => row.length > 0);
+  const LIMITS = { red: 12, green: 13, blue: 14 };
+
+  const regGame = /Game ([0-9]*)\:/;
+  const regColor = / ([0-9]*) (red|green|blue)/g;
+  const games = input.map(x => {
+    const id = eval(x.match(regGame)[1]);
+    let colors = { red: 0, blue: 0, green: 0 };
+    [...x.matchAll(regColor)]
+        .forEach(y => { if (colors[y[2]] < eval(y[1])) colors[y[2]] = eval(y[1]) });
+    return { id, colors };
+  });
+
+  const sum = games
+      .filter(x => Object.keys(LIMITS).reduce((a,c) => a && (LIMITS[c] >= x.colors[c]), true))
+      .map(x => x.id)
+      .reduce((a,x) => a+x, 0);
+
+  console.log("E1: " + sum);
+});
