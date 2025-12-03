dc26999c6e8c6e3b4bdf35991d539943023e84fe
Tuomas Paasonen <tpaasone@gmail.com>
2025-12-03 19:26:31 +0200

    Ignore empty lines in the input

--- 2025/_/e01.py
+++ 2025/_/e01.py
@@ -5,5 +5,7 @@ FILE = "test.txt" if "-t" in sys.argv else "input.txt"
 input = []
 with open(FILE, "r") as f:
     for r in f:
-        input.append(r.replace("\n", ""))
+        r = r.replace("\n", "")
+        if len(r) > 0:
+            input.append(r.replace("\n", ""))
 
